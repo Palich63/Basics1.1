b39ded1c6e4ed1ac54815d5df994a77066ae8b57
Andrey Globa <Globa.Andrey@gmail.com>
2020-06-28 17:28:28 +0300

    to progress

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
 public class CashbackHackServiceTest {
 
     CashbackHackService test = new CashbackHackService();
